0a5c949c8c237a97faf39dd8e764fa12c51c790f
HectorBarotto <fxhector@outlook.com>
2023-03-19 09:11:17 -0300

    direccion web

--- src/main/java/com/yoprogramo/portfolio/WebConfig.java
+++ src/main/java/com/yoprogramo/portfolio/WebConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.util.pattern.PathPattern;
 
 @EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("https://portfolio-hmb.web.app/");
     }
 }
